e8ba03c7dc5e038813dcd6e27aef66dbcbfe080b
achp <hadixakbar@gmail.com>
2015-10-12 19:25:11 +0700

    remove print url ,add progress bar & count total size

--- crawler.py
+++ crawler.py
@@ -1,33 +1,40 @@
 __author__ = 'xhadix'
-import requests, urllib
+import requests, urllib2
 from bs4 import BeautifulSoup
 from urlparse import urlparse
+from progressbar import ProgressBar
 
 print "usage insert URL with quotes \"google.com\" "
 url = input("insert url to crawl = ")
 r = requests.get('http://' + url)
-
-html = r.content
+pbar = ProgressBar()
+html = r.text
 parse = BeautifulSoup(html, 'html.parser')
 if r.status_code == 200:
     totalImg = parse.findAll('img')
     print 'total image in site = ', len(totalImg)
-    for imgSrc in totalImg:
+    for imgSrc in pbar(totalImg):
+        totalSize = 0
         cekUrl = urlparse(imgSrc['src'])
 
-        if cekUrl.scheme == '' and cekUrl.netloc != '':
+        if cekUrl.scheme == '' and cekUrl.netloc != '' and  not imgSrc['src'].startswith('//'):
             imageSize = 'http://'+imgSrc['src']
-        elif cekUrl.scheme == '' and cekUrl.netloc == '':
+        elif cekUrl.scheme == '' and cekUrl.netloc == '' and not imgSrc["src"].startswith("images/"):
             imageSize = 'http://'+url+imgSrc['src']
+        elif cekUrl.netloc == '' and cekUrl.scheme == '' and imgSrc["src"].startswith("images/"):
+            imageSize = 'http://'+url+'/'+imgSrc['src']
+        elif imgSrc['src'].startswith('//'):
+            imageSize = 'http:'+imgSrc['src']
         else:
             imageSize = imgSrc['src']
 
         try:
-            print(imageSize)
-            fileSize = urllib.urlopen(imageSize)
-            print(fileSize.headers.get("content-length"))
+            fileSize = urllib2.urlopen(imageSize)
+            totalSize += int(fileSize.headers.get("content-length"))
         except Exception as e:
             print('cannot open link '+str(e))
+
+    print(totalSize)
 
 else:
     print('access blocked')
